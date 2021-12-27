7cbf9445d5146a4a44f2f21665c00eaacd6ec23c
henishpatel9045 <200120107182@git.org.in>
2021-12-27 17:44:49 +0530

    First Commit

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import EmptyScreen from "./app/screens/EmptyScreen";
+import Screen from "./app/screens/Screen";
+import { dark } from "./app/configs/themes";
+import NotesScreen from "./app/screens/NotesScreen";
+import DocScreen from "./app/screens/DocScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <DocScreen />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: dark.primary,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
